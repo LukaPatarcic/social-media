191b4817187a6f4d37e142b313bbc035e1e32171
LukaPatarcic <patarcic98@gmail.com>
2019-12-10 14:09:31 +0100

    Simple login form android

--- mobile/App.js
+++ mobile/App.js
@@ -1,66 +1,100 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {
-    SafeAreaView,
-    StyleSheet,
-    ScrollView,
-    View,
-    Text,
-    Button,
-    TouchableHighlight,
-    Alert,
-    StatusBar,
-} from 'react-native';
+import {Text, View, Button, StyleSheet, Alert, TextInput, Image, AsyncStorage} from 'react-native';
 import BootstrapStyleSheet from 'react-native-bootstrap-styles';
-
 
 const bootstrapStyleSheet = new BootstrapStyleSheet();
 const s = bootstrapStyleSheet.create();
 const c = bootstrapStyleSheet.constants;
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ecf0f1',
+    },
+    input: {
+        width: 200,
+        height: 44,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 10,
+    },
+});
+
+
 export default class App extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            message: ''
+            email: '',
+            password: '',
+            error: '',
+            token: '',
+        };
+    }
+
+    onLogin() {
+        const { email, password, error } = this.state;
+        if(email === '' || password === '') {
+            this.setState({
+                error: 'Please enter all fields'
+            })
+        } else {
+            fetch('http://api.allshak.lukaku.tech/login',{
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify({email,password})
+            })
+                .then((response => response.json()))
+                .then((data => {
+                    this.setState({error: data.error});
+                    if(data.token) {
+                        this.setState({error: 'Success',token: data.token})
+                    }
+                }))
         }
 
     }
 
-
-    componentDidMount() {
-        fetch('http://electrowallet.lukaku.tech/test')
-            .then(response => response.json())
-            .then(data => {
-                this.setState({message: data.message})
-            })
-            .catch(err => {
-                this.setState({message: "Error"})
-            })
-    }
-
-    onPress() {
-        Alert.alert('Hello World','Hello World');
-    }
-
     render() {
+        const {error,email,password,token} = this.state;
+        const color = error === 'Success' ? 'green' : 'red';
         return (
-            <View style={[s.body]}>
-                <TouchableHighlight onPress={this.onPress} style={[s.btnTouchable]}>
-                    <View style={[s.btn, s.btnPrimary]}>
-                        <Text style={[s.btnText, s.btnTextPrimary]}>Signup</Text>
-                    </View>
-                </TouchableHighlight>
+            <View style={styles.container}>
+                <Image
+                    style={{width: '100%', height: 100, margin: 30}}
+                    source={{uri: 'http://allshak.lukaku.tech/images/logo.png'}}
+                />
+                <TextInput
+                    value={email}
+                    onChangeText={(email) => this.setState({ email })}
+                    placeholder={'Email'}
+                    style={styles.input}
+                />
+                <TextInput
+                    value={password}
+                    onChangeText={(password) => this.setState({ password })}
+                    placeholder={'Password'}
+                    secureTextEntry={true}
+                    style={styles.input}
+                />
+                <Text style={{color: color}}>
+                    {error ? error : ''}
+                </Text>
+
+                <Button
+                    title={'Login'}
+                    style={styles.input}
+                    onPress={this.onLogin.bind(this)}
+                />
+                <Text style={{marginTop: 20, textAlign: 'center'}}>{error === 'Success' ? token : ''}</Text>
             </View>
         );
     }
-
 }
-
